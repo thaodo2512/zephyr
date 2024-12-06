edefe9272869961d66c24d0802f124136c09da59
Thao Do <thao.do2512@hcmut.edu.vn>
2024-12-06 23:55:33 +0700

    grivers: add motor_drive driver
    Signed-off-by: Thao Do <thao.do2512@hcmut.edu.vn>

---
+++ drivers/motor_drive/l298.c
@@ -0,0 +1,198 @@
+/**
+ * Copyright (c) 2024 Thao Do
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+#include <stdint.h>
+#include <zephyr/drivers/motor_drive.h>
+#include <zephyr/drivers/pwm.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(motor_driver);
+
+#define DT_DRV_COMPAT thao_do_motor_drive
+
+struct motor_drive_config {
+	int num_motors;
+	const struct pwm_dt_spec motor;
+	const struct gpio_dt_spec pin_io_0;
+	const struct gpio_dt_spec pin_io_1;
+};
+
+static __unused int motor_pwm_set_output(const struct device *dev, uint32_t motor, uint32_t percent)
+{
+	struct motor_drive_config *config;
+	uint32_t period;
+
+	if (!dev) {
+		return -EINVAL;
+	}
+
+	config = (struct motor_drive_config *)dev->config;
+	if (!config) {
+		assert_print("%s: config should be not NULL\n", __func__);
+	}
+
+	if (motor > config->num_motors) {
+		return -EINVAL;
+	}
+
+	const struct pwm_dt_spec *dt_motor = &config->motor;
+
+	// covert percent to pulse_nsec
+	period = (dt_motor->period * percent) / 100;
+
+	return pwm_set_pulse_dt(dt_motor, period);
+}
+
+static int motor_get_inf(const struct device *dev, uint32_t motor, const struct motor_info **info)
+{
+	ARG_UNUSED(dev);
+	ARG_UNUSED(motor);
+	ARG_UNUSED(info);
+
+	return 0;
+}
+
+static int motor_set_speed(const struct device *dev, uint32_t motor, uint8_t value)
+{
+	return 0;
+}
+
+static int motor_drive_on(const struct device *dev, uint32_t motor, uint8_t dir)
+{
+	struct motor_drive_config *cfg;
+	int rc;
+
+	if (!dev) {
+		return -EINVAL;
+	}
+
+	cfg = (struct motor_drive_config *)dev->config;
+	if (!cfg) {
+		assert_print("%s: config should be not NULL\n", __func__);
+	}
+
+	if (motor > cfg->num_motors) {
+		return -EINVAL;
+	}
+
+	if (dir == MOTOR_DIRVE_DIRECTION_FORWARD) {
+		rc = gpio_pin_set_dt(&cfg->pin_io_0, 1);
+		if (rc) {
+			return rc;
+		}
+		rc = gpio_pin_set_dt(&cfg->pin_io_1, 0);
+	} else {
+		rc = gpio_pin_set_dt(&cfg->pin_io_0, 0);
+		if (rc) {
+			return rc;
+		}
+		rc = gpio_pin_set_dt(&cfg->pin_io_1, 1);
+	}
+
+	return 0;
+}
+
+static int motor_drive_off(const struct device *dev, uint32_t motor)
+{
+	struct motor_drive_config *cfg;
+	int rc = 0;
+
+	if (!dev) {
+		return -EINVAL;
+	}
+
+	cfg = (struct motor_drive_config *)dev->config;
+	if (!cfg) {
+		assert_print("%s: config should be not NULL\n", __func__);
+	}
+
+	if (motor > cfg->num_motors) {
+		return -EINVAL;
+	}
+
+	rc = gpio_pin_set_dt(&cfg->pin_io_0, 1);
+	rc = gpio_pin_set_dt(&cfg->pin_io_0, 0);
+	if (rc) {
+		return rc;
+	}
+	rc = gpio_pin_set_dt(&cfg->pin_io_0, 1);
+	rc = gpio_pin_set_dt(&cfg->pin_io_0, 0);
+	if (rc) {
+		return rc;
+	}
+
+	return rc;
+}
+
+static int motor_pwm_init(const struct device *dev)
+{
+	struct motor_drive_config *config = (struct motor_drive_config *)dev->config;
+	int rc;
+
+	if (!config->num_motors) {
+		LOG_ERR("%s: no motors found (DT child nodes missing)", dev->name);
+		return -ENODEV;
+	}
+
+	if (!gpio_is_ready_dt(&config->pin_io_0) || !gpio_is_ready_dt(&config->pin_io_1)) {
+		LOG_ERR("%s: gpio device not ready", dev->name);
+		return -ENODEV;
+	}
+
+	rc = gpio_pin_configure_dt(&config->pin_io_0, GPIO_OUTPUT_INACTIVE);
+	if (rc) {
+		return rc;
+	}
+	rc = gpio_pin_configure_dt(&config->pin_io_1, GPIO_OUTPUT_INACTIVE);
+	if (rc) {
+		return rc;
+	}
+
+	return 0;
+}
+
+static struct motor_driver_api motor_driver_api = {
+	.on = motor_drive_on,
+	.off = motor_drive_off,
+	.set_speed = motor_set_speed,
+	.get_info = motor_get_inf,
+};
+
+#if IS_ENABLED(CONFIG_PWM)
+#define MOTOR_DRIVE_GET_PWM_DEVICE(inst) PWM_DT_SPEC_GET(DT_DRV_INST(inst))
+#else
+#define MOTOR_DRIVE_GET_PWM_DEVICE(inst)                                                           \
+	{                                                                                          \
+		0                                                                                  \
+	}
+#endif // CONFIG_PWM
+
+#if IS_ENABLED(CONFIG_GPIO)
+
+#define MOTOR_DRIVE_GET_GPIO(inst, pro_name) GPIO_DT_SPEC_GET(DT_INST_CHILD(inst, pro_name), gpios)
+
+#else
+
+#define MOTOR_DRIVE_GET_GPIO(inst, pro_name)                                                       \
+	{                                                                                          \
+		0                                                                                  \
+	}
+
+#endif // CONFIG_GPIO
+
+#define MOTOR_PWM_DEVICE(inst)                                                                     \
+	static struct motor_drive_config motor_drive_config##inst = {                              \
+		.num_motors = 1,                                                                   \
+		.motor = MOTOR_DRIVE_GET_PWM_DEVICE(inst),                                         \
+		.pin_io_0 = MOTOR_DRIVE_GET_GPIO(inst, gpio_ctrl0),                                \
+		.pin_io_0 = MOTOR_DRIVE_GET_GPIO(inst, gpio_ctrl1),                                \
+	};                                                                                         \
+	DEVICE_DT_INST_DEFINE(inst, motor_pwm_init, NULL, NULL, &motor_drive_config##inst,         \
+			      POST_KERNEL, CONFIG_MOTOR_DRIVE_DRIVER_INIT_PRIORITY, &motor_driver_api);
+
+DT_INST_FOREACH_STATUS_OKAY(MOTOR_PWM_DEVICE)
---
+++ include/zephyr/drivers/motor_drive.h
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2024 Thao Do
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef MOTOR_DRIVE_H
+#define MOTOR_DRIVE_H
+
+#include <errno.h>
+#include <zephyr/types.h>
+#include <zephyr/device.h>
+
+#define MOTOR_DIRVE_DIRECTION_FORWARD  0
+#define MOTOR_DIRVE_DIRECTION_BACKWARD 1
+
+enum motor_drive_status {
+	MOTOR_DRIVE_STATUS_OFF = 0,
+	MOTOR_DRIVE_STATUS_ON,
+	MOTOR_DRIVE_STATUS_ERROR,
+};
+
+struct motor_info {
+	const char *label;
+	uint32_t index;
+	enum motor_drive_status status;
+};
+
+/**
+ * @brief This API helps to turn on the motor which the specific direction
+ *
+ * @param dev Pointer to the device structure for the driver instance.
+ * @param motor The motor index
+ * @param dir The direction of the motor (0: forward, 1: backward)
+ * @retval int 0 if successful, negative errno code on failure.
+ */
+typedef int (*motor_drive_on_t)(const struct device *dev, uint32_t motor, uint8_t dir);
+
+/**
+ * @brief This API helps to turn off the motor
+ *        Force off by turning off 2 control GPIOs
+ *
+ * @param dev   Pointer to the device structure for the driver instance.
+ * @param motor The motor index
+ * @retval int 0 if successful, negative errno code on failure.
+ */
+typedef int (*motor_drive_off_t)(const struct device *dev, uint32_t motor);
+
+/**
+ * @brief This API helps to get the motor information
+ *
+ */
+typedef int (*motor_drive_get_info_t)(const struct device *dev, uint32_t motor,
+				      const struct motor_info **info);
+
+/** @brief Set the speed of the motor
+ *
+ * @param dev Pointer to the device structure for the driver instance.
+ * @param motor The motor index
+ * @param value The speed value
+ * @retval int 0 if successful, negative errno code on failure.
+ */
+typedef int (*motor_drive_set_speed_t)(const struct device *dev, uint32_t motor, uint8_t value);
+
+struct motor_driver_api {
+	motor_drive_on_t on;
+	motor_drive_off_t off;
+	motor_drive_set_speed_t set_speed;
+	motor_drive_get_info_t get_info;
+};
+
+#endif /* MOTOR_DRIVE_H */
